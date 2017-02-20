fedcb0dbf1b2af9322cc11fdd4729dbf07d6fa5e
Anthony Centurione <tcentur@gmail.com>
2017-02-20 17:00:25 -0500

    About page

--- routes.py
+++ routes.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@applroute("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
 
