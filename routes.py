06b0062d08058c05ae8ed20c29828e3d3987ab8f
Anthony Centurione <tcentur@gmail.com>
2017-02-20 17:08:19 -0500

    Fix typos

--- routes.py
+++ routes.py
@@ -6,11 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@applroute("/about")
+@app.route("/about")
 def about():
     return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
-
-
