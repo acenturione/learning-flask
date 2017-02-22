50f257f84014c94fea8e9b8b5622086e3e71f6c6
Anthony Centurione <tcentur@gmail.com>
2017-02-21 22:46:39 -0500

    Fix typos

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from flask.ext.sqlalchemy import SQLAlchemy
-from werkzeug import gererate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
