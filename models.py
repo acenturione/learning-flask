eefdc00a83d749591cadd631840cade9ee0de60b
Anthony Centurione <tcentur@gmail.com>
2017-02-21 22:38:37 -0500

    Fix typos

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     __tablename__ = 'users'
     uid = db.Column(db.Integer, primary_key = True)
     firstname = db.Column(db.String(100))
-    lastname = db.Column(db.String(120)), unique = True)
+    lastname = db.Column(db.String(120), unique = True)
     pwdhash = db.Column(db.String(54))
 
     def __init__(self, firstname, lastname, email, password):
